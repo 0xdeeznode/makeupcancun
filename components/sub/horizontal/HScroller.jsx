ee4734933864752502a08973aceb9cd8a87b6cb8
0xdeeznode <nalanarud@gmail.com>
2024-02-02 22:58:07 -0500

    about1 and about2 divs done restyling and responsive

--- components/sub/horizontal/HScroller.jsx
+++ components/sub/horizontal/HScroller.jsx
@@ -102,31 +102,35 @@ const About2 = () => {
 
   return (
     <>
-      <div className='grid grid-cols-3 grid-rows-3 gap-y-0 sm:gap-y-14 h-screen m-0 py-5 w-[100vw] bg-rojo'>
-        <div className='col-span-3 row-span-2 sm:col-span-2 sm:row-span-1 flex items-end h-full sm:h-fit gap-5 text-blanco pl-5 font-playFair'>
+      <div className='grid grid-cols-3 grid-rows-4 h-screen m-0 py-5 w-[100vw] bg-rojo border-2'>
+
+        <div className='col-span-3 row-span-2 sm:col-span-2 sm:row-span-2 flex h-full sm:h-fit gap-5 text-blanco px-4 font-playFair border border-green-700'>
           <p className='text-xl sm:text-2xl'>{AboutText.second.main}</p>
-          <p className='text-xl'>{AboutText.second.sub}</p>
+          <p className='text-lg sm:text-2xl'>{AboutText.second.sub}</p>
         </div>
-        <div className='hidden sm:flex col-span-1 row-span-3'>
+
+        <div className='hidden sm:flex col-span-1 row-span-4 h-full border border-blue-700'>
           <div className='w-[470px] h-[562px]'>
             <Image
               src='/about2d2.jpg'
               width={640}
               height={800}
-              alt='Fashion week ny model makeup'
+              alt='Fashion Week NY 2023 kids makeup'
             />
           </div>
         </div>
-        <div className='col-span-3 row-span-1 sm:col-span-2 sm:row-span-2 flex justify-center items-end h-full'>
-          <div className='w-[642px] h-[321px]'>
+        
+        <div className='col-span-3 row-span-2 sm:col-span-2 sm:row-span-2 flex justify-center h-full pt-5 sm:pt-0 border border-blue-700'>
+          <div className='sm:w-[545px] h-[321px] my-auto'>
             <Image
               src='/about2d3.jpg'
               width={640}
               height={319}
-              alt='Fashion week ny model makeup'
+              alt='Fashion Week NY 2023 model makeup'
             />
           </div>
         </div>
+
       </div>
     </>
   )
@@ -136,7 +140,7 @@ const About = () => {
 
   return (
     <>
-      <div className='grid grid-cols-3 grid-rows-3 sm:gap-x-4 h-screen w-[100vw] bg-rojo'>
+      <div className='grid grid-cols-3 grid-rows-3 sm:gap-x-4 h-screen py-5 w-[100vw] bg-rojo'>
 
         <div className='col-span-3 row-span-1 sm:col-span-1 sm:row-span-3 pl-2 sm:pl-6 sm:pt-6 flex sm:justify-center sm:items-center text-blanco font-playFair'>
           <ul className='w-full'>
