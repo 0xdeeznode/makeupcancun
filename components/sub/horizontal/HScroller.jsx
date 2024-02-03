342ae7ff2cca7a05b004d94c30483e642e7731ad
0xdeeznode <nalanarud@gmail.com>
2024-02-02 21:41:38 -0500

    horizontal scroll functionallity working responsive

--- components/sub/horizontal/HScroller.jsx
+++ components/sub/horizontal/HScroller.jsx
@@ -19,32 +19,81 @@ const HScroller = () => {
   }
 
   gsap.registerPlugin(ScrollTrigger);
-  const component = useRef(null);
+  const componentH = useRef(null);
 
   useEffect(() => {
     let ctx = gsap.context(() => {
       const tl = gsap.timeline({
         scrollTrigger: {
-
+          trigger:componentH.current,
+          start:'top top',
+          end: () => {`+=${getScrollAmount() * -1}`},
+          pin:true,
+          scrub:1,
+          invalidateOnRefresh:true,
         },
       });
-    }, component)
+
+      tl.to('#races', {
+        x: getScrollAmount,
+        duration: 3,
+        ease: "none",
+      })
+
+    }, componentH)
 
     return () => ctx.revert()  // Clean up
   }, [races])
 
   return (
     <>
-      <div id="races-wrapper" className='flex w-full'>
-        <div id='races' className="flex w-full">
-          <div>
+      <div className="h-[50vh]" />
+
+      <div ref={componentH}>
+        <div id="races-wrapper">
+          <div id='races' className="flex w-fit flex-nowrap">
             <About />
             <About2 />
+            <About3 />
           </div>
         </div>
       </div>
-
+      
       <div className="h-[100vh]" />
+    </>
+  )
+}
+
+const About3 = () => {
+
+  return (
+    <>
+      <div className='grid grid-cols-3 grid-rows-3 gap-y-0 sm:gap-y-14 h-screen m-0 py-5 w-[100vw] bg-rojo'>
+        <div className='col-span-3 row-span-2 sm:col-span-2 sm:row-span-1 flex items-end h-full sm:h-fit gap-5 text-blanco pl-5 font-playFair'>
+          <p className='text-xl sm:text-2xl'>{AboutText.second.main}</p>
+          <p className='text-xl'>{AboutText.second.sub}</p>
+        </div>
+        <div className='hidden sm:flex col-span-1 row-span-3'>
+          <div className='w-[470px] h-[562px]'>
+            <Image
+              src='/about2d2.jpg'
+              width={640}
+              height={800}
+              alt='Fashion week ny model makeup'
+            />
+          </div>
+        </div>
+        <div className='col-span-3 row-span-1 sm:col-span-2 sm:row-span-2 flex justify-center items-end h-full'>
+          <div className='w-[642px] h-[321px]'>
+            <Image
+              src='/about2d3.jpg'
+              width={640}
+              height={319}
+              alt='Fashion week ny model makeup'
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
@@ -52,62 +101,76 @@ const HScroller = () => {
 const About2 = () => {
 
   return (
-    <div>
-      <div className='grid grid-cols-3 grid-rows-3 gap-y-0 sm:gap-y-14 h-screen my-auto py-5'>
-
+    <>
+      <div className='grid grid-cols-3 grid-rows-3 gap-y-0 sm:gap-y-14 h-screen m-0 py-5 w-[100vw] bg-rojo'>
         <div className='col-span-3 row-span-2 sm:col-span-2 sm:row-span-1 flex items-end h-full sm:h-fit gap-5 text-blanco pl-5 font-playFair'>
           <p className='text-xl sm:text-2xl'>{AboutText.second.main}</p>
           <p className='text-xl'>{AboutText.second.sub}</p>
         </div>
         <div className='hidden sm:flex col-span-1 row-span-3'>
-          <Image 
-            src='/about2d2.jpg'
-            width={420}
-            height={400}
-            alt='Fashion week ny model makeup'
-          />
+          <div className='w-[470px] h-[562px]'>
+            <Image
+              src='/about2d2.jpg'
+              width={640}
+              height={800}
+              alt='Fashion week ny model makeup'
+            />
+          </div>
         </div>
         <div className='col-span-3 row-span-1 sm:col-span-2 sm:row-span-2 flex justify-center items-end h-full'>
-          <Image 
-            src='/about2d3.jpg'
-            width={620}
-            height={400}
-            alt='Fashion week ny model makeup'
-          />
+          <div className='w-[642px] h-[321px]'>
+            <Image
+              src='/about2d3.jpg'
+              width={640}
+              height={319}
+              alt='Fashion week ny model makeup'
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
-
 
 const About = () => {
 
   return (
-    <div>
-      <div className='grid grid-cols-3 grid-rows-3 sm:gap-x-4 h-screen'>
-        <div className='col-span-3 sm:col-span-2 row-span-1 pl-6 pt-10 sm:pt-6 flex flex-col sm:flex-row gap-6 text-blanco font-playFair'>
-          <h1 className='text-4xl sm:text-5xl'>About Maria Fernanda Montealegre</h1>
-          <p className='text-xl sm:text-3xl'>{AboutText.firts.main}</p>
+    <>
+      <div className='grid grid-cols-3 grid-rows-3 sm:gap-x-4 h-screen w-[100vw] bg-rojo'>
+
+        <div className='col-span-3 row-span-1 sm:col-span-1 sm:row-span-3 pl-2 sm:pl-6 sm:pt-6 flex sm:justify-center sm:items-center text-blanco font-playFair'>
+          <ul className='w-full'>
+            <li className='text-6xl sm:text-8xl'>About</li>
+            <li className='text-6xl sm:text-7xl'>Maria Fernanda </li>
+            <li className='text-6xl sm:text-7xl'>Montealegre</li>
+          </ul>
         </div>
-        <div className='col-span-3 row-span-3 p-4 sm:p-0 sm:col-span-1 sm:row-span-3'>
-          <Image 
-          src='/mfmAbout2.png'
-          alt='Makeup artist Mafer Montealegre picture'
-          width={860}
-          height={450}
-          />
+        
+
+        <div className='col-span-3 row-span-2 sm:col-span-1 sm:row-span-3 flex flex-col gap-5 items-center'>
+          <p className='text-xl sm:text-3xl text-center text-blanco font-playFair'>{AboutText.firts.main}</p>
+          <div className='w-[400px] h-[400px] mx-auto'>
+            <Image
+            src='/mfmAbout.jpg'
+            alt='Makeup artist Mafer Montealegre picture two'
+            width={400}
+            height={400}
+            />
+          </div>
         </div>
-        <div className='col-span-2 row-span-2 hidden sm:flex justify-end'>
-          <Image 
-          src='/mfmAbout.jpg'
-          alt='Makeup artist Mafer Montealegre picture two'
-          width={400}
-          height={400}
-          />
+
+        <div className='hidden sm:flex col-span-1 row-span-3'>
+          <div className='w-[430px] h-[573px]'>
+            <Image
+            src='/mfmAbout2.png'
+            alt='Makeup artist Mafer Montealegre picture'
+            width={640}
+            height={853}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- components/sub/horizontal/Portada.jsx
+++ components/sub/horizontal/Portada.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Portada = () => {
   return (
     <>
-        <div className='flex justify-start py-5 px-10'>
+        <div className='flex justify-start pt-5 px-2 sm:px-10'>
             <div className='flex w-1/2 text-start items-center'>
                 <h1 className='text-blanco text-5xl sm:text-[82px] font-bold font-playFair'>TRANSFORMING BEAUTY, ONE EVENT AT A TIME. WE CREATE ELEGANCE, NOT JUST MAKEUP</h1>
             </div>
