0df65d7117f0b5d7503f2b64d8b7fb0c9e02259d
0xdeeznode <nalanarud@gmail.com>
2024-02-09 13:28:48 -0500

    add new images

--- components/main/Weddings.jsx
+++ components/main/Weddings.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
+'use client'
+import { useState } from 'react';
 import { WeddingsInfo } from '@/constants'
 import Image from 'next/image'
-import Link from 'next/link'
 
 const Weddings = () => {
+
+    const [isHovered, setIsHovered] = useState(false);
+
+
+
   return (
-    <div className="grid grid-cols-2 gap-3 sm:gap-14 h-full w-full px-2 sm:px-4 lg:px-10">
+    <div className="grid grid-cols-2 gap-8 sm:gap-14 h-full w-full px-2 sm:px-4 lg:px-10">
         {/* Col1 */}
-        <div className="flex flex-col items-center gap-20 col-span-1">
+        <div className="flex flex-col items-center gap-14 lg:gap-20 col-span-1">
             <div className="flex flex-col gap-4 text-black font-playFair">
                 <h4 className="text-2xl sm:text-4xl">{WeddingsInfo.first.main}</h4>
                 <p className='lg:text-lg'>{WeddingsInfo.first.sub}</p>
             </div>
 
-            <div className="h-fit w-fit border-4 border-black object-contain object-center overflow-hidden">
+            <div className="h-fit w-fit border-4 border-black object-contain object-center overflow-hidden"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
                 <Image className="w-[500px] object-cover transition-transform hover:scale-110 ease-in-out duration-500" 
-                src='/servicesBrides.jpg'
+                src={isHovered ? '/servicesBridesAlt.jpg' : '/servicesBrides.jpg'}
                 alt='Wedding at the beach makeup cancun'
                 width={837}
                 height={559}
@@ -30,7 +38,7 @@ const Weddings = () => {
         </div>
 
         {/* Col2 */}
-        <div className="flex flex-col items-center gap-20 sm:gap-16 col-span-1">
+        <div className="flex flex-col items-center gap-14 lg:gap-20 col-span-1">
 
             <div className="h-fit w-fit border-4 border-black object-contain overflow-hidden">
                 <Image className="w-[500px] object-cover transition-transform hover:scale-110 ease-in-out duration-500" 
